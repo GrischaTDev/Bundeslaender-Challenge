bfa52fc76c0364f20ca9f99f6565e0603fb7096d
GrischaTDev <grischa.taenzer@gmail.com>
2024-01-05 22:11:59 +0100

    Kleines Update

--- script.js
+++ script.js
@@ -22,28 +22,4 @@ function renderBundeslaender(responseAsJson) {
         `;
         
     }
-
-
-    let bundeslandListe = document.getElementById('bundeslaender');
 }
-
-
-
-
-
-// async function loadCourse() {
-//     let url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=' + API_KEY;  // Anstatt die Variable mit dem API Kex per ${} hinzuzufügen, können wir die Variable auch mit + anhängen.
-//     let response = await fetch(url); // response übersetzt ANTWORT. Hier rufen wir die Daten von der API ab.
-//     let responseAsJson = await response.json(); // Dann wandeln wir die daten in ein JSON um.
-//     let bitcoinPrice = (Math.round(responseAsJson['Realtime Currency Exchange Rate']['5. Exchange Rate']));
-//     let lastRefresh = (responseAsJson['Realtime Currency Exchange Rate']['6. Last Refreshed']);
-
-//     // console.log(Math.round(responseAsJson['Realtime Currency Exchange Rate']['5. Exchange Rate']));
-
-//     document.getElementById('course').innerHTML = /* html */ `
-//     <div class="btc-price">${bitcoinPrice} $</div>
-//     <span>Letzes Update</span>
-//     <div class="last-refresh">${lastRefresh}</div> 
-//     `;
-//     loadMonthlyCourse();
-// }
